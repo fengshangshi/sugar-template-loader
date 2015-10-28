5cccc66dfe32e28d7885eb5c71ed7dd2444da1ab
冯尚实 <fengshangshi@fengshangshideMacBook-Pro.local>
2015-10-28 13:41:12 +0800

    dev

---
+++ index.js
@@ -0,0 +1,23 @@
+/**
+ * @file 加载模板
+ * @author fengshangshi
+ */
+var _ = require('lodash');
+
+var Scaffold = module.exports = function(opts) {
+	this.options = _.merge({}, opts);
+};
+
+Scaffold.prototype.download = function(id, cb, progress) {
+	var supports = ['github'];
+	var type = this.options['type'];
+	if (!~supports.indexOf(type)) {
+		type = 'github';
+	}
+	var request = new (require('./lib/' + type))(this.options);
+	request.download(id, cb, progress);
+};
+
+Scaffold.prototype.deliver = function(from, to) {
+
+};
